35a759ae391e4d33bd8d1c8ae05840aba97e609c
Pazos Damian <dpazos@andreani.com>
2022-02-10 00:25:17 -0300

    Terminado

---
+++ Ejercicio N°2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Analice la siguiente porción de código para calcular la sumatoria de 1 a 10. ¿Cuál es el error? ¿Qué hace realmente?
+             * int sum=0; 
+             * int i=1;
+             * while (i<=10); 
+             * { 
+             * sum += i++;
+             * }
+             */
+
+            // Resolucion
+
+            int sum = 0; 
+            int i = 1;
+            while (i != 11) // En este sector se encuentra un ; que genera que la sentencia while se quede sin su bloque de codigo
+            {
+                sum += i++;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
