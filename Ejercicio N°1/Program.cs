44a1e0381884d54d854c9987caf251249db7a2a0
Pazos Damian <dpazos@andreani.com>
2022-02-10 00:14:15 -0300

    Terminado

--- Ejercicio N°1/Program.cs
+++ Ejercicio N°1/Program.cs
@@ -10,7 +10,22 @@ namespace Ejercicio_N_1
     {
         static void Main(string[] args)
         {
+            /*
+             * ¿Cuál es el problema del código siguiente y cómo se soluciona? 
+             * int i=0;
+             * for(int i=1;i<=10;) {
+             *      Console.WriteLine(i++); 
+             *                      }
+             * Console.ReadKey();
+            */
 
+            // Resolucion de consigna
+
+            for (int i=1;i<=10;i++) // Declaro e inicializo la variable 'i' con el valor correspondiente - Ademas le agrego el parametro que utiliza el for que faltaba
+            {
+                Console.WriteLine(i); // Le quito la suma
+            }
+            Console.ReadKey();
         }
     }
 }
